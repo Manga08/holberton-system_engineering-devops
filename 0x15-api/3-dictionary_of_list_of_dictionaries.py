242d3d62b66373b1357fc15016f88bbdc36ac707
Manga08 <bijuu5445@gmail.com>
2020-09-14 17:48:41 -0500

    [Fix] The 3 Task

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
 """Using a REST API, for a given employee ID"""
-import json
-import requests
 
+if __name__ == '__main__':
+    import requests
+    import json
+    from sys import argv
 
-if __name__ == "__main__":
-    URL = 'https://jsonplaceholder.typicode.com/'
-    URL_User = URL + 'users/'
-    resp_User = requests.get(URL_User)
-    users = resp_User.json()
-    users_dicty = {}
-    for current_user in users:
-        URL = URL + 'todos'
-        resp = requests.get(URL)
-        employee = current_user.get('id')
-        data = [info for info in resp.json()
-                if info.get('userId') is int(employee)]
-        list = []
-        for info in data:
-            new_dicty = {"username": current_user['username'],
-                         "task": info['title'],
-                         "completed": info['completed'],
-                         }
-            list.append(new_dicty)
-        users_dicty[current_user.get('id')] = list
+    all_id = set()
+    resp = requests.get('https://jsonplaceholder.typicode.com/posts')
+    data = resp.json()
+    for user in data:
+        all_id.add(user.get('userId'))
 
-    with open('{}.json'.format('todo_all_employees'), 'w') as f:
-        json.dump(users_dicty, f)
+    export = {}
+    for user in all_id:
+        resp = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                            format(user))
+        user = resp.json().get('username')
+
+        resp = requests.get('https://jsonplaceholder.typicode.com/' +
+                            'todos?userId={}'.format(user))
+        data = resp.json()
+
+        export['{}'.format(user)] = []
+        for task in data:
+            export['{}'.format(user)].append({
+                'task': task.get('title'),
+                'completed': task.get('completed'),
+                'username': user
+            })
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump({int(x): export[x] for x in export.keys()},
+                  f, sort_keys=True)
