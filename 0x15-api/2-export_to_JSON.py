d3a684196917bbaa58352e0321d95f6ba0dec0c4
Manga08 <bijuu5445@gmail.com>
2020-09-14 17:48:34 -0500

    [Fix] The 2 Task

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
 """Using a REST API, for a given employee ID,export data in the JSON format."""
-import requests
-from sys import argv
-import json
-import requests
-import sys
 
+if __name__ == '__main__':
+    import requests
+    import json
+    from sys import argv
 
-if __name__ == "__main__":
-    employee = sys.argv[1]
-    URL = 'https://jsonplaceholder.typicode.com/'
-    URL_User = URL + 'users/' + employee
-    resp = requests.get(URL_User)
-    Name = resp.json().get('username')
-    URL_User = URL + 'todos'
-    resp = requests.get(URL_User)
-    info = [task for task in resp.json()
-            if task.get('userId') is int(employee)]
-    listy = []
+    resp = requests.get('https://jsonplaceholder.typicode.com/users/{}'.
+                        format(argv[1]))
+    user = resp.json().get('username')
 
-    for data in info:
-        new_dict = {"task": data['title'],
-                    "completed": data['completed'],
-                    "username": Name}
-        listy.append(new_dict)
-    dicty = {sys.argv[1]: listy}
-    with open('{}.json'.format(sys.argv[1]), 'w') as f:
-        json.dump(dicty, f)
+    resp = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.
+                        format(argv[1]))
+    data = resp.json()
+
+    export = {}
+    export['{}'.format(argv[1])] = []
+    for task in data:
+        export['{}'.format(argv[1])].append({
+            'task': task.get('title'),
+            'completed': task.get('completed'),
+            'username': user
+        })
+    with open('{}.json'.format(argv[1]), 'w') as f:
+        json.dump(export, f)
